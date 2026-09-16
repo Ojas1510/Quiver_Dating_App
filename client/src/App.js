47d1b20d2de5b21e301ed6dc8bdc1c78014bf421
Ojas Nagta <109330348+Ojas1510@users.noreply.github.com>
2026-09-16 20:53:48 +0530

    Fix unused cookie variables in App

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import OnBoarding from "./pages/OnBoarding";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Cookies, useCookies } from "react-cookie";
+import { useCookies } from "react-cookie";
 
 const App = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  const [cookies] = useCookies(["user"]);
   const authToken = cookies.AuthToken;
 
   return (
