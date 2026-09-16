8ae0d7c84313dce453d0103048cfc17e12ab02be
Ojas Nagta <109330348+Ojas1510@users.noreply.github.com>
2026-09-16 20:56:32 +0530

    Fix unused cookie variables in Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,18 @@
 import Nav from "../components/Nav.js";
 import { useState } from "react";
 import AuthModal from "../components/AuthModal.js";
-import { Cookies, useCookies } from "react-cookie";
+import { useCookies } from "react-cookie";
 
 const Home = () => {
   const [showModal, setShowModal] = useState(false);
   const [isSignUp, setIsSignUp] = useState(true);
-  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  const [cookies, , removeCookie] = useCookies(["user"]);
   const authToken = cookies.AuthToken;
 
   const handleClick = () => {
     if (authToken) {
-      removeCookie("UserId", cookies.UserId);
-      removeCookie("AuthToken", cookies.AuthToken);
+      removeCookie("UserId");
+      removeCookie("AuthToken");
       window.location.reload();
       return;
     }
@@ -22,24 +22,16 @@ const Home = () => {
 
   return (
     <div className="overlay">
-      <Nav
-        minimal={false}
-        authToken={authToken}
-        setShowModal={setShowModal}
-        showModal={showModal}
-        setIsSignUp={setIsSignUp}
-      />
+      <Nav minimal={false} authToken={authToken} setShowModal={setShowModal} showModal={showModal} setIsSignUp={setIsSignUp} />
       <div className="home">
-        <h1 className="primary-title"> Find Your Spark in a Swipe</h1>
-
+        <h1 className="primary-title">Find Your Spark in a Swipe</h1>
         <button className="primary-button" onClick={handleClick}>
           {authToken ? "Signout" : "Create Account"}
         </button>
-        {showModal && (
-          <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
-        )}
+        {showModal && <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />}
       </div>
     </div>
   );
 };
+
 export default Home;
